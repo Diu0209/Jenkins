140b9106fc9e423e0e19b751d55c3936a8c828f0
Diu0209 <150352788+Diu0209@users.noreply.github.com>
2024-03-18 17:57:06 +0530

    clicked

--- src/test/java/basic/Organization_Test.java
+++ src/test/java/basic/Organization_Test.java
@@ -1,16 +1,51 @@
 package basic;
 
+import java.io.IOException;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import commonUtils.PropertyFileUtil;
+import commonUtils.WebDriverUtil;
+
 public class Organization_Test
 {
 	@Test
-	public void CreateOrganizationsTest()
+	public void CreateOrganizationsTest() throws IOException, InterruptedException
 	{
+		
+		
+		
+		PropertyFileUtil putil = new PropertyFileUtil();
+		WebDriverUtil wutil = new WebDriverUtil();
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("http://localhost:8888/");
+		
+			
+			//To maximize the window.(call method from WebDriverUtil class)
+			wutil.Maximize(driver);
+			
+			
+			//To read data from property file.
+			String URL=putil.getDataFromPropertyFile("Url");
+			
+			//To read data from property file.
+			String USER=putil.getDataFromPropertyFile("Username");
+			
+			//To read data from property file.
+			String PASS=putil.getDataFromPropertyFile("Password");
+			
+			
+			//To display url & enter the username & password from external property file.
+			driver.get(URL);
+			wutil.ImpliciteWait(driver);
+			driver.findElement(By.name("user_name")).sendKeys(USER);
+			driver.findElement(By.name("user_password")).sendKeys(PASS);
+			Thread.sleep(4000);
+			
+			//To click on login button.
+			driver.findElement(By.id("submitButton")).click();
+			
 	}
 }
